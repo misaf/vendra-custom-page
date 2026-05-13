62236f864e4f691357c942a236dc396b4c404ebd
Misaf <misaf.1990@gmail.com>
2026-05-13 13:07:10 +0330

    feat: add German translations

---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'custom_page'                     => 'Benutzerdefinierte Seiten',
+    'custom_page_category'            => 'Benutzerdefinierte Seitenkategorien',
+    'custom_page_category_management' => 'Verwaltung benutzerdefinierter Seitenkategorien',
+    'custom_page_management'          => 'Verwaltung benutzerdefinierter Seiten',
+];
