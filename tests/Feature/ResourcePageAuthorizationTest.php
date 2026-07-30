7881e91f765e2caf02da43c599aff83680189941
misaf <misaf.1990@gmail.com>
2026-07-30 13:51:43 +0000

    Merge remote-tracking branch 'origin/1.x' into 1.x

--- tests/Feature/ResourcePageAuthorizationTest.php
+++ tests/Feature/ResourcePageAuthorizationTest.php
@@ -16,7 +16,7 @@ use Misaf\VendraCustomPage\Filament\Clusters\Resources\CustomPages\Pages\ViewCus
 use function Pest\Livewire\livewire;
 
 beforeEach(function (): void {
-    setUpFilamentSuperAdminTestContext();
+    setUpFilamentAdminTestContext();
 
     Filament::getPanel('admin')->plugin(
         SpatieTranslatablePlugin::make()->defaultLocales(['en', 'de']),
