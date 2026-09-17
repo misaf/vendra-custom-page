028bd864f434b37e22727062060a7121f50afffe
misaf <misaf.1990@gmail.com>
2026-09-17 19:49:15 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace Misaf\VendraCustomPage\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Misaf\VendraCustomPage\CustomPagePlugin;
 use Misaf\VendraCustomPage\Database\Seeders\DemoContentSeeder;
 use Misaf\VendraCustomPage\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 
 #[Description('Seed custom page module data for a tenant')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed custom page data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = CustomPagePlugin::ID;
-
-    protected $signature = self::MODULE_NAME.':seed
-        {tenant? : Tenant ID or slug to seed custom page data for}
-        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}';
 
     /**
      * @return array<string, class-string>
