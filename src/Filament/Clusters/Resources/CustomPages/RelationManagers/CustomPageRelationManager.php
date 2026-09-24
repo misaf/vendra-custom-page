04bad7d4832bf0b26913dfea1a7f1c0ef54b8ce5
misaf <misaf.1990@gmail.com>
2026-09-24 13:07:41 +0000

    feat(store): mark a store's primary domain

--- src/Filament/Clusters/Resources/CustomPages/RelationManagers/CustomPageRelationManager.php
+++ src/Filament/Clusters/Resources/CustomPages/RelationManagers/CustomPageRelationManager.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Misaf\VendraCustomPage\Filament\Clusters\Resources\CustomPages\RelationManagers;
 
+use BackedEnum;
 use Filament\Actions\CreateAction;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Number;
@@ -23,6 +25,8 @@ final class CustomPageRelationManager extends RelationManager
     public ?string $activeLocale = null;
 
     protected static string $relationship = 'customPages';
+
+    protected static string|BackedEnum|null $icon = Heroicon::OutlinedDocument;
 
     protected static bool $isBadgeDeferred = true;
 
